3b3359347877540f00ae8006c5495cc4912e0913
William Lima Pereira <william.pereira@totvs.com.br>
2013-03-28 09:36:24 -0300

    versão para module em snodejs

--- demo/js/flowchartConnectorsDemo.js
+++ demo/js/flowchartConnectorsDemo.js
@@ -1,17 +1,11 @@
 ;(function() {
 	
 	window.jsPlumbDemo = {
-		init : function() {
-				
-			jsPlumb.importDefaults({
-				// default drag options
-				DragOptions : { cursor: 'pointer', zIndex:2000 },
-				// default to blue at one end and green at the other
-				EndpointStyles : [{ fillStyle:'#225588' }, { fillStyle:'#558822' }],
-				// blue endpoints 7 px; green endpoints 11.
-				Endpoints : [ [ "Dot", {radius:7} ], [ "Dot", { radius:11 } ]],
-				// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
-				// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+		init : function() {				
+			jsPlumb.importDefaults({				
+				DragOptions : { cursor: 'pointer', zIndex:2000 },				
+				EndpointStyles : [{ fillStyle:'#225588' }, { fillStyle:'#558822' }],				
+				Endpoints : [ [ "Dot", {radius:7} ], [ "Dot", { radius:11 } ]],				
 				ConnectionOverlays : [
 					[ "Arrow", { location:0.9 } ],
 					[ "Label", { 
@@ -22,7 +16,7 @@
 				]
 			});			
 
-			// this is the paint style for the connecting lines..
+			
 			var connectorPaintStyle = {
 				lineWidth:5,
 				strokeStyle:"#deea18",
@@ -30,12 +24,12 @@
 				outlineColor:"#EAEDEF",
 				outlineWidth:7
 			},
-			// .. and this is the hover style. 
+			
 			connectorHoverStyle = {
 				lineWidth:7,
 				strokeStyle:"#2e2aF8"
 			},
-			// the definition of source endpoints (the small blue ones)
+			
 			sourceEndpoint = {
 				endpoint:"Dot",
 				paintStyle:{ fillStyle:"#225588",radius:7 },
@@ -53,9 +47,7 @@
 	                } ]
                 ]
 			},
-			// a source endpoint that sits at BottomCenter
-		//	bottomSource = jsPlumb.extend( { anchor:"BottomCenter" }, sourceEndpoint),
-			// the definition of target endpoints (will appear when the user drags a connection) 
+		
 			targetEndpoint = {
 				endpoint:"Dot",					
 				paintStyle:{ fillStyle:"#558822",radius:11 },
@@ -87,34 +79,26 @@
 					}
 				};
 
-			_addEndpoints("window4", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);			
+			_addEndpoints("window5", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
+			_addEndpoints("window4", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
 			_addEndpoints("window2", ["LeftMiddle", "BottomCenter"], ["TopCenter", "RightMiddle"]);
 			_addEndpoints("window3", ["RightMiddle", "BottomCenter"], ["LeftMiddle", "TopCenter"]);
-			_addEndpoints("window1", ["LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter"]);
-						
-			// listen for new connections; initialise them the same way we initialise the connections at startup.
+			_addEndpoints("window1", ["LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter"]);						
+			
 			jsPlumb.bind("jsPlumbConnection", function(connInfo, originalEvent) { 
 				init(connInfo.connection);
 			});			
 						
-			// make all the window divs draggable						
+			
 			jsPlumb.draggable(jsPlumb.getSelector(".window"), { grid: [20, 20] });
-			// THIS DEMO ONLY USES getSelector FOR CONVENIENCE. Use your library's appropriate selector method!
-			//jsPlumb.draggable(jsPlumb.getSelector(".window"));
-
-            
-			// connect a few up
+			
 			jsPlumb.connect({uuids:["window2BottomCenter", "window3TopCenter"], editable:true});
 			jsPlumb.connect({uuids:["window2LeftMiddle", "window4LeftMiddle"], editable:true});
 			jsPlumb.connect({uuids:["window4TopCenter", "window4RightMiddle"], editable:true});
 			jsPlumb.connect({uuids:["window3RightMiddle", "window2RightMiddle"], editable:true});
 			jsPlumb.connect({uuids:["window4BottomCenter", "window1TopCenter"], editable:true});
 			jsPlumb.connect({uuids:["window3BottomCenter", "window1BottomCenter"], editable:true});
-			//
-            
-			//
-			// listen for clicks on connections, and offer to delete connections on click.
-			//
+			
 			jsPlumb.bind("click", function(conn, originalEvent) {
 				if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
 					jsPlumb.detach(conn); 
