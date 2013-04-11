b3ac239be722c3ee4145e64db1b68e2fe835e0d9
William Lima Pereira <william.pereira@totvs.com.br>
2013-04-11 16:18:07 -0300

    gerando os require do arquivo js final

--- routes/build.js
+++ routes/build.js
@@ -1,16 +1,40 @@
 var fs = require('fs');
 
 var allModules = [];
+var allInstanceSource = [];
+var allInstanceTarget = [];
 
 exports.buildOrchestrator = function(pathProject, fun) {
   var file = fs.readFileSync(pathProject+'workspace/1.diag', 'utf8');
   var diagrama = JSON.parse(file);
   console.log('diagrama name : '+diagrama.name);
 
-  for(var i in diagrama.content){  	
+  clearFileJS(pathProject);
+
+  for(var i in diagrama.content){
   	putModule(diagrama.content[i].sourceId);
-  	putModule(diagrama.content[i].targetId);
-  	appendCallMethod(pathProject, getModuloFromInstance(diagrama.content[i].targetId), getModuloFromInstance(diagrama.content[i].targetId), 'next');  	
+  	putModule(diagrama.content[i].targetId);  	
+  }
+
+  for(var i in diagrama.content){
+  	putInstanceSource(diagrama.content[i].sourceId);  	 	
+  }
+
+  for(var i in diagrama.content){
+  	putInstanceTarget(diagrama.content[i].targetId);  	 	
+  }
+
+  //cria os require
+  for(var i in allModules){
+  	appendRequire(pathProject, getModuloFromInstance(allModules[i]));
+  }
+
+  //cria os method
+  for(var i in allInstanceSource){
+  	appendCallMethod(pathProject
+  		, getModuloFromInstance(allInstanceSource[i])
+  		, getModuloFromInstance(allInstanceSource[i])
+  		, getModuloFromInstance(allInstanceTarget[i]));
   }
 
   fun();
@@ -18,6 +42,28 @@ exports.buildOrchestrator = function(pathProject, fun) {
 
 var getModuloFromInstance = function(instance) {
 	return instance.replace(/^instance-\d-(.*)$/, '$1');
+};
+
+var putInstanceTarget = function(module) {
+	var _module = module;
+	for(var i in allInstanceTarget){
+		if(allInstanceTarget[i] == _module){
+			return true;
+		}
+	}
+	allInstanceTarget.push(_module);
+	return false;
+};
+
+var putInstanceSource = function(module) {
+	var _module = module;
+	for(var i in allInstanceSource){
+		if(allInstanceSource[i] == _module){
+			return true;
+		}
+	}
+	allInstanceSource.push(_module);
+	return false;
 };
 
 var putModule = function(module) {
@@ -31,26 +77,29 @@ var putModule = function(module) {
 	return false;
 };
 
+var clearFileJS = function(pathProject) {
+	fs.writeFileSync(pathProject+'workspace/1.js', '');
+};
+
 var appendCallMethod = function(pathProject, method, box, nextMethod) {
 	var fileJS = fs.readFileSync(pathProject+'workspace/1.js', 'utf8');
 	var appendContent = tplMethod.replace(/\$method\$/, method).replace(/\$box\$/, box).replace(/\$nextMethod\$/, nextMethod);
 
-	fs.writeFile(pathProject+'workspace/1.js', fileJS+appendContent, function(err) {      
-      if(err) 
-        console.error(err);              
-
-        console.log('Arquivo JS salvo com sucesso');      
-    });
+	fs.writeFileSync(pathProject+'workspace/1.js', fileJS+appendContent);
 };
 
-var tplMethod = "\nexports.$method$ = function(data) {\n"+
-				"\t$box$.init(data, function(args) {\n"+
-					"\t\texports.$nextMethod$(args);\n"+
-				"\t});\n"+
-			"};\n";
+var appendRequire = function(pathProject, box) {
+	var fileJS = fs.readFileSync(pathProject+'workspace/1.js', 'utf8');
+	var appendContent = tplRequire.replace(/\$box\$/, box).replace(/\$box\$/, box);
+
+	fs.writeFileSync(pathProject+'workspace/1.js', fileJS+appendContent);
+};
+
+var tplRequire = "var $box$ = require('$box$/index.js');\n";
 
 var tplMethod = "\nexports.$method$ = function(data) {\n"+
 				"\t$box$.init(data, function(args) {\n"+
 					"\t\texports.$nextMethod$(args);\n"+
 				"\t});\n"+
 			"};\n";
+
--- workspace/1.js
+++ workspace/1.js
@@ -1,18 +1,22 @@
+var formularioDeEntrada = require('formularioDeEntrada/index.js');
+var somarvalores = require('somarvalores/index.js');
+var convertTotal = require('convertTotal/index.js');
+var enviarEmail = require('enviarEmail/index.js');
 
-exports.constructor = function(data) {
-	conversor.init(data, function(args) {
-		exports.somar(args);
-	});
-};
-
-exports.instance-1-somarvalores = function(data) {
-	instance-1-somarvalores.init(data, function(args) {
-		exports.next(args);
+exports.formularioDeEntrada = function(data) {
+	formularioDeEntrada.init(data, function(args) {
+		exports.somarvalores(args);
 	});
 };
 
 exports.somarvalores = function(data) {
 	somarvalores.init(data, function(args) {
-		exports.next(args);
+		exports.convertTotal(args);
+	});
+};
+
+exports.convertTotal = function(data) {
+	convertTotal.init(data, function(args) {
+		exports.enviarEmail(args);
 	});
 };
